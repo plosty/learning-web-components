c9d77198184ce5d7517c39965483bf93aba7695c
Phil Losty <phillosty@hotmail.com>
2017-10-11 08:57:02 +0100

    updating product page

--- product-page/product.js
+++ product-page/product.js
@@ -1,5 +1,4 @@
 document.getElementById("display-checkout-button").addEventListener("click", toggleCart);
-document.getElementById("product-listing").addEventListener("click", addToCart);
 
 function toggleCart() {
   var cart = document.getElementById("cart");
@@ -7,12 +6,15 @@ function toggleCart() {
   if (cart.className==="split-right hidden") {
     products.className="split-left";
     cart.className="split-right";
+    calculateTotal();
   }
   else {
     products.className="split-left w100";
     cart.className="split-right hidden";
   }
 }
+
+document.getElementById("product-listing").addEventListener("click", addToCart);
 
 function addToCart(buttonId) {
   var cart = document.getElementById("cart");
@@ -23,8 +25,30 @@ function addToCart(buttonId) {
   }
 
   cartProducts.insertAdjacentHTML( 'afterbegin',
-    '<div class="product-cart-listing"><div class="product-image"><img src="images/product-1.jpg" alt="Product Image" /></div><div class="product-text"><div class="product-name">Pinarello Gan 2017 Shimano 105 Mix Carbon/Sky </div><div class="product-price">£1,920.00</div><div class="product-quantity-row"><label for="product-quantity">Quantity</label><input type="text" name="product-quantity" class="product-quantity" placeholder="1" /></div><input type="button" class="remove-buttonbutton" value="Remove Item" /></div></div>'
+    '<div class="product-cart-listing"><div class="product-cart-image"><img src="images/product-1.jpg" alt="Product Image" /></div><div class="product-cart-text"><div class="product-cart-name">Pinarello Gan 2017 Shimano 105 Mix Carbon/Sky </div><div>£<span class="product-cart-price">1920.00</span></div><div class="product-cart-quantity-row"><label for="product-cart-quantity">Quantity</label><input type="text" name="product-cart-quantity" class="product-cart-quantity" placeholder="1" /></div><input type="button" class="product-cart-remove-button button" value="Remove Item" /></div></div>'
   );
+
+  calculateTotal();
+}
+
+var cartProducts = document.getElementsByClassName("product-cart-remove-button");
+for (i=0; i<cartProducts.length; i++) {
+  cartProducts[i].addEventListener("click", removeProduct);
+}
+
+function removeProduct() {
+  var cartProductContainer = document.getElementById("cart-products");
+  cartProductContainer.removeChild(cartProductContainer.childNodes[0]);
+  calculateTotal();
+}
+
+var cartProductQuantities = document.getElementsByClassName("product-cart-quantity");
+for (i=0; i<cartProductQuantities.length; i++) {
+  cartProducts[i].addEventListener("change", changeProductQuantity);
+}
+
+function changeProductQuantity() {
+  calculateTotal();
 }
 
 function cartIsEmpty() {
@@ -41,15 +65,11 @@ document.getElementById("cart-products").addEventListener("change", calculateTot
 
 function calculateTotal() {
   var cartPrices = document.getElementsByClassName("product-cart-price");
-  alert("array: " + cartPrices + " length: " + cartPrices.length);
-  var totalPrice = 0;
+    var totalPrice = 0;
   for (var i=0; i<cartPrices.length; i++) {
     cartValue=parseFloat(cartPrices[i].innerText);
     totalPrice+=cartValue;
   }
-  alert("totalPrice: " + totalPrice);
-  alert("totalPrice: " + totalPrice.toFixed(2));
-  
   document.getElementById("total-price-amount").innerText = totalPrice.toFixed(2);
 }
 
@@ -58,23 +78,23 @@ document.getElementById("apply-voucher-button").addEventListener("click", applyD
 function applyDiscountCode() {
   var currentAmount=document.getElementById("total-price-amount").innerText;
   var discountCode=document.getElementById("voucher-code");
-  
+
   var discountAmount = 0;
 
   switch(discountCode.value) {
     case("DISCOUNT_10"):
       discountAmount = 0.1;
       discountCode.value="";
-      break;      
+      break;
     case("DISCOUNT_5"):
       discountAmount = 0.05;
       discountCode.value="";
-    break;      
+    break;
     default:
       discountCode.value="Code Not Found";
       break;
   }
 
-  currentAmount=currentAmount-(currentAmount*discountAmount);  
+  currentAmount=currentAmount-(currentAmount*discountAmount);
   document.getElementById("total-price-amount").innerText = currentAmount.toFixed(2);
 }
