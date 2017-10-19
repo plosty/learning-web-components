7b53d19d52efbb867ed2698b3fb16cd7348bfb1d
Phil Losty <phillosty@hotmail.com>
2017-10-19 21:36:17 +0100

    adding tabs JS file

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -2,7 +2,24 @@
 var tabs = (function(window) {  
   var tabs = {};
 
-  
+  var departmentLinks = document.getElementsByClassName("departmentLink");
+  for (i=0; i<departmentLinks.length; i++) {
+    departmentLinks[i].addEventListener("click", openContent);
+  }
 
+  function openContent() {
+    var headerNode = this;
+    var departmentLinks = document.getElementsByClassName("departmentLink");
+    for (i=0; i<departmentLinks.length; i++) {     
+      if (departmentLinks[i]===headerNode) {      
+        headerNode.className=("departmentLink active");
+        headerNode.nextSibling.nextSibling.className=("content");
+      }
+      else {
+        departmentLinks[i].className=("departmentLink");
+        departmentLinks[i].nextSibling.nextSibling.className=("content hidden");
+      }
+    }
+  }
   return tabs;
 })(window)
