530acb09c80234f8a19a6cea2fb7e738bdf0c055
Phil Losty <phillosty@hotmail.com>
2017-10-20 13:15:54 +0100

    updated accordion to use Javascript

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -1,13 +1,21 @@
-var accordion = (function(window) {  
+var accordion = (function(window) {
   var accordion = {};
 
   var menuLinks = document.getElementsByClassName("ac-label");
   for (i=0; i<menuLinks.length; i++) {
-    menuLinks[i].addEventListener("click", openSection);
+    menuLinks[i].addEventListener("click", toggleSection);
   }
 
-  function openSection() {
-    this.previousSibling.checked = "true";
+  function toggleSection() {
+    var menuHeader = this;
+    if (menuHeader.classList.contains("selected")){
+      menuHeader.className = "ac-label";
+      menuHeader.nextElementSibling.className = "content";
+    }
+    else {
+      menuHeader.className = "ac-label selected";
+      menuHeader.nextElementSibling.className = "content active";
+    }
   }
 
   return accordion;
