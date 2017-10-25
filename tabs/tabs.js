cc1c91ca83f9f2089cbcc77ee7dce50431ecdfaa
Phil Losty <phillosty@hotmail.com>
2017-10-25 17:17:21 +0100

    updated tabs component to resolve ul / li / div issue

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,24 +1,23 @@
-var tabs = (function(window) {  
-  var tabs = {};
+(function(window) {  
 
-  var departmentLinks = document.getElementsByClassName("departmentLink");
-  for (i=0; i<departmentLinks.length; i++) {
-    departmentLinks[i].addEventListener("click", openContent);
+  var tabs = document.getElementsByClassName("tab");
+
+  for (i=0; i<tabs.length; i++) {
+    tabs[i].addEventListener("click", openContent);
   }
 
   function openContent() {
-    var headerNode = this;
-    var departmentLinks = document.getElementsByClassName("departmentLink");
-    for (i=0; i<departmentLinks.length; i++) {     
-      if (departmentLinks[i]===headerNode) {      
-        headerNode.className=("departmentLink active");
-        headerNode.nextSibling.nextSibling.className=("content");
+    var tabNum = parseInt(this.id);
+    var contentSections = document.getElementsByClassName("content");
+    for (i=0; i<tabs.length; i++) {   
+      if (i === tabNum) {      
+        this.className=("tab active");
+        contentSections[i].className="content";
       }
       else {
-        departmentLinks[i].className=("departmentLink");
-        departmentLinks[i].nextSibling.nextSibling.className=("content hidden");
+        tabs[i].className=("tab");
+        contentSections[i].className=("content hidden");
       }
     }
   }
-  return tabs;
-})(window)
+})()
