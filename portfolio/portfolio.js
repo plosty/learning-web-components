1ef396b5680fac47e5a8c791798920adffe16895
Phil Losty <phillosty@hotmail.com>
2017-11-25 22:03:21 +0000

    added hambureger menu for reduced width screen

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,3 +1,21 @@
+/* Hamburger Navigation */
+var hamburgerIcon = document.getElementsByClassName("hamburger-icon");
+var hamburgerMenu = document.getElementsByClassName("narrow-dropdown-menu");
+var hamburgerLinks = document.getElementsByClassName("hamburger-section-link");
+
+hamburgerIcon[0].addEventListener("click", function () {
+  hamburgerMenu[0].classList.toggle("hamburger-menu-displayed");
+});
+
+for (var i=0; i<hamburgerLinks.length; i++) {
+  hamburgerLinks[i].addEventListener("click", function () {
+    hamburgerMenu[0].classList.toggle("hamburger-menu-displayed");
+  });
+}
+
+
+
+
 /* Main site navigation */
 var headerSections = document.getElementsByClassName("section-link");
 
