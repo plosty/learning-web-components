209c0cfa54b06c045f65d741ace9e0ef5735d153
Phil Losty <phillosty@hotmail.com>
2018-04-27 23:25:11 +0100

    setting up files

---
+++ jigsaw/puzzle-script.js
@@ -0,0 +1,51 @@
+(function() {
+  "use strict";
+
+  window.addEventListener("onload", arrangePieces);
+
+  var arrangePieces = function() {};
+
+  var pieces = [
+    "Group11.png",
+    "Group14.png",
+    "Group17.png",
+    "Group20.png",
+    "Group23.png",
+    "Group26.png",
+    "Group29.png",
+    "Group32.png",
+    "Group35.png",
+    "Group38.png",
+    "Group41.png",
+    "Group44.png",
+    "Group6.png,",
+    "Group9.png",
+    "Group12.png",
+    "Group15.png",
+    "Group18.png",
+    "Group21.png",
+    "Group24.png",
+    "Group27.png",
+    "Group30.png",
+    "Group33.png",
+    "Group36.png",
+    "Group39.png",
+    "Group42.png",
+    "Group45.png",
+    "Group7.png",
+    "Group10.png",
+    "Group13.png",
+    "Group16.png",
+    "Group19.png",
+    "Group22.png",
+    "Group25.png",
+    "Group28.png",
+    "Group31.png",
+    "Group34.png",
+    "Group37.png",
+    "Group40.png",
+    "Group43.png",
+    "Group5.png",
+    "Group8.png"
+  ];
+})();
